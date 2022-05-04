0ec765f44f965975408e3e145b78c7178ce9186d
Marty Sweet <marty.sweet@omnistratus.co.uk>
2022-05-04 18:08:57 +0100

    cleanup install script

--- ros2/dev_ws/rpi_fetch_and_run.sh
+++ ros2/dev_ws/rpi_fetch_and_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # WGET this file on the RPI and execute it
-# $ wget https://raw.githubusercontent.com/martysweet/lawn-mower/main/ros2/dev_ws/rpi_fetch_and_run.sh
+# $ wget https://raw.githubusercontent.com/martysweet/lawn-mower/main/ros2/dev_ws/rpi_fetch_and_run.sh -o rpi_fetch_and_run.sh
 # $ chmod +x rpi_fetch_and_run.sh && bash rpi_fetch_and_run.sh install
 # bash rpi_fetch_and_run.sh install
 
@@ -10,9 +10,7 @@ if [ $1 = "install" ]; then
   curl -fsSL https://get.docker.com -o get-docker.sh
   sudo sh get-docker.sh
   sudo usermod -aG docker pi
-  sudo apt-get install libffi-dev libssl-dev
-  sudo apt install python3-dev
-  sudo apt-get install -y python3 python3-pip
+  sudo apt-get install libffi-dev libssl-dev python3 python3-pip python3-dev -y
   sudo pip3 install docker-compose
   sudo systemctl enable docker
   echo "DONE! Now run bash to reinitialise your terminal. Then bash rpi_fetch_and_run.sh"
