e17321290f8b3cbfae920d4902a0b4333fd826a6
Marty Sweet <marty.sweet@omnistratus.co.uk>
2022-05-04 18:07:31 +0100

    Add docker compose

--- ros2/dev_ws/rpi_fetch_and_run.sh
+++ ros2/dev_ws/rpi_fetch_and_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # WGET this file on the RPI and execute it
-# wget https://raw.githubusercontent.com/martysweet/lawn-mower/main/ros2/dev_ws/rpi_fetch_and_run.sh
-# chmod +x rpi_fetch_and_run.sh
+# $ wget https://raw.githubusercontent.com/martysweet/lawn-mower/main/ros2/dev_ws/rpi_fetch_and_run.sh
+# $ chmod +x rpi_fetch_and_run.sh && bash rpi_fetch_and_run.sh install
 # bash rpi_fetch_and_run.sh install
 
 # Ensure docker is installed
@@ -10,7 +10,12 @@ if [ $1 = "install" ]; then
   curl -fsSL https://get.docker.com -o get-docker.sh
   sudo sh get-docker.sh
   sudo usermod -aG docker pi
-  echo "DONE! Now run bash to reinitialise your terminal"
+  sudo apt-get install libffi-dev libssl-dev
+  sudo apt install python3-dev
+  sudo apt-get install -y python3 python3-pip
+  sudo pip3 install docker-compose
+  sudo systemctl enable docker
+  echo "DONE! Now run bash to reinitialise your terminal. Then bash rpi_fetch_and_run.sh"
   exit
 fi
 
